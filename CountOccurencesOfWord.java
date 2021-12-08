4e0151af062a506ea19f1e3a5e67eaec75f1eb6e
Rishabh Nigam <65018070+rishabhnigam007@users.noreply.github.com>
2021-12-08 11:00:59 +0530

    Add files via upload

---
+++ CountOccurencesOfWord.java
@@ -0,0 +1,50 @@
+//string a="aabcadbbbca";
+import java.util.*;
+class CountOccurencesOfWord
+{
+	public static void main(String ar[])
+	{
+		String a="aabcadbbbca";
+		int count=0;
+		
+		/*for(int i=0;i<a.length();i++)
+		{
+			char c=a.charAt(i);
+			// counting done
+			//-----------------------------
+			for(int j=0;j<a.length();j++)
+			{
+				if(c==a.charAt(j))
+				{
+					count++;
+				}
+			}
+			// counting print but unique
+			
+		
+				System.out.println(c+" total : "+count);
+				count=0;
+			
+		}*/
+
+		HashMap<Character,Integer> hm=new HashMap<Character,Integer>();
+		char c[]=a.toCharArray();
+		for(char c1:c)
+		{
+			if(hm.containsKey(c1))
+			{
+				hm.put(c1,hm.get(c1)+1);
+			}
+			else
+			{
+				hm.put(c1,1);
+			}
+		}
+		for(Map.Entry entry:hm.entrySet())
+		{
+			System.out.println(entry.getKey()+"-"+entry.getValue());
+		}
+	}
+}
+
+
